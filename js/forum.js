901af084f0c402b169b65261b2e0e633a1c58bcb
sesileka <sesilina.lane@gmail.com>
2026-06-12 08:11:58 -0700

    Added in inline svg for forum page.

--- js/forum.js
+++ js/forum.js
@@ -4,11 +4,12 @@ const forum_categories = [
     {category: 'New Member Introductions', description: 'Introduce yourself to the community!'},
     {category: 'Stories from the Trailhead', description: 'Left the trail with an interesting experience and a story to tell? Share with the Great Outdoors community!'},
     {category: 'Photo Gallery', description: 'Posting on IG not covering it anymore? Drop your best hiking pics here.'},
-    {category: 'Off Topic', icon: '🌿', description: 'Discuss and share your thoughts outside of trails and hiking.'}
+    {category: 'Off Topic', description: 'Discuss and share your thoughts outside of trails and hiking.'}
 ]
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('ic-users').innerHTML = sizeIcon('users',56);
+    document.getElementById('discussion').innerHTML = sizeIcon('discussion',28);
 
 
 document.getElementById('principles-grid').innerHTML = forum_categories.map(f => (`
